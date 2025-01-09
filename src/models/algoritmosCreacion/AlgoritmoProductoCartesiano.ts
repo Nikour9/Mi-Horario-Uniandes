cafae3b4c25769b52a07baed33fc698db5fd8c08
Nikour9 <nicolasuriber10@gmail.com>
2025-01-09 16:43:05 -0500

    Update AlgoritmoProductoCartesiano.ts
    Se actualiza para usar el algortimo de backtracking en lugar del producto cartesiano para hacer el programa mas eficiente.

--- src/models/algoritmosCreacion/AlgoritmoProductoCartesiano.ts
+++ src/models/algoritmosCreacion/AlgoritmoProductoCartesiano.ts
@@ -2,14 +2,35 @@ import AlgoritmoCreacionHorarios from "./AlgoritmoCreacionHorarios";
 import Horario from "../Horario";
 import Seccion from "../Seccion";
 import Curso from "../Curso";
-import {horarioEsValido} from "@/services/operacionesSobreHorario";
-import { obtenerBloquesGuardados } from "@/services/almacenamiento/almacenamientoBloques";
+import { horarioEsValido } from "@/services/operacionesSobreHorario";
 
-export default class AlgoritmoProductoCartesiano implements AlgoritmoCreacionHorarios {
-  crearHorarios(cursos: Curso[]): Horario[] {
-    const seccionesPorCurso = cursos.map(curso => curso.secciones);
-    const combinacionesSeccionesDeCurso = seccionesPorCurso.reduce<Seccion[][]>((combinaciones, cursoActual) => {return combinaciones.flatMap(a => cursoActual.map(c => [...a, c]));}, [[]]);
-    const horarios = combinacionesSeccionesDeCurso.map((combinacion,index) => new Horario(index,combinacion)).filter(horario => horarioEsValido(horario));
-    return horarios;
-  }
+export default class AlgoritmoBacktracking implements AlgoritmoCreacionHorarios {
+    crearHorarios(cursos: Curso[]): Horario[] {
+        const horarios: Horario[] = [];
+        this.backtrack(cursos, 0, [], horarios);
+        return horarios;
+    }
+
+    private backtrack(
+        cursos: Curso[],
+        indiceCurso: number,
+        seccionesActuales: Seccion[],
+        horarios: Horario[]
+    ): void {
+        if (indiceCurso === cursos.length) {
+            // Base case: We've assigned a section to each course
+            const nuevoHorario = new Horario(horarios.length, [...seccionesActuales]);
+            if (horarioEsValido(nuevoHorario)) {
+                horarios.push(nuevoHorario);
+            }
+            return;
+        }
+
+        const cursoActual = cursos[indiceCurso];
+        for (const seccion of cursoActual.secciones) {
+            seccionesActuales.push(seccion);
+            this.backtrack(cursos, indiceCurso + 1, seccionesActuales, horarios);
+            seccionesActuales.pop(); // Backtrack: remove the current section
+        }
+    }
 }
